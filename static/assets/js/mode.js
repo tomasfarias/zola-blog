425c9be1251e347db152997a65d705932cdd040f
Tomás Farías Santana <tomas@tomasfarias.dev>
2021-08-13 01:25:38 +0200

    feat: Styling changes all around

--- static/assets/js/mode.js
+++ static/assets/js/mode.js
@@ -1,18 +1,21 @@
 // In case you cannot tell, I don't do JS
-if (localStorage.getItem('theme') === 'dark' || localStorage.getItem('theme') === null) {
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+const btn = document.querySelector(".btn");
+
+if (localStorage.getItem('theme') === 'dark' || prefersDarkScheme.matches) {
     document.body.className = 'dark';
 } else {
     document.body.className = 'light';
 }
 
-document.getElementById('mode').addEventListener('click', () => {
-    if (localStorage.getItem('theme') === 'light' || localStorage.getItem('theme') === null) {
-        document.body.classList.remove('light');
-        document.body.classList.add('dark');
-        localStorage.setItem('theme', 'dark');
-    } else {
+btn.addEventListener('click', () => {
+    if (localStorage.getItem('theme') === 'dark' || prefersDarkScheme.matches) {
         document.body.classList.add('light');
         document.body.classList.remove('dark');
         localStorage.setItem('theme', 'light');
+    } else {
+        document.body.classList.remove('light');
+        document.body.classList.add('dark');
+        localStorage.setItem('theme', 'dark');
     }
 });
