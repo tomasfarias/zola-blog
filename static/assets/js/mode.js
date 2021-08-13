94a509b6de8437b7d4d0ba90987c2843832b4779
Tomás Farías Santana <tomas@tomasfarias.dev>
2021-08-13 19:05:55 +0200

    fix: More style changes

--- static/assets/js/mode.js
+++ static/assets/js/mode.js
@@ -1,21 +1,26 @@
 // In case you cannot tell, I don't do JS
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 const btn = document.querySelector(".btn");
+const theme = document.querySelector("#syntax-theme");
 
 if (localStorage.getItem('theme') === 'dark' || prefersDarkScheme.matches) {
     document.body.className = 'dark';
+    theme.href = "syntax-theme-dark.css";
 } else {
     document.body.className = 'light';
+    theme.href = "syntax-theme-light.css";
 }
 
 btn.addEventListener('click', () => {
     if (localStorage.getItem('theme') === 'dark' || prefersDarkScheme.matches) {
         document.body.classList.add('light');
         document.body.classList.remove('dark');
+        theme.href = "syntax-theme-light.css";
         localStorage.setItem('theme', 'light');
     } else {
         document.body.classList.remove('light');
         document.body.classList.add('dark');
+        theme.href = "syntax-theme-dark.css";
         localStorage.setItem('theme', 'dark');
     }
 });
